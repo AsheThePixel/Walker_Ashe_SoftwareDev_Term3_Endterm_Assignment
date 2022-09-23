8a5cb9b42a71102db1863f1d6f5f230694bc0145
AsheThePixel <ashe.ian.walker@Gmail.com>
2022-09-23 13:15:18 +0200

    Walker_Ashe_SoftwareDev_Term3_Endterm_Assignment

---
+++ js/array-literal.js
@@ -0,0 +1,16 @@
+//1. Create an array called colors and assign it values to "white", "black", "Green";
+
+
+
+//2. Show the first item from the array using their index number.
+
+
+//remember to use innerHTML
+
+const colors = [];
+colors[0]= "White";
+colors[1]= "Black";
+colors[2]= "Green";
+
+document.getElementById("colors").innerHTML = colors[2];
+
---
+++ js/numeric-variable.js
@@ -0,0 +1,17 @@
+// create three variables to store the information needed, namely:
+
+// 1.the price of each letter = 5
+// 2. the quantity = 15
+// 3. calculate the total by multipying the price by quantity
+
+//4. Get the element with an id "cost"
+
+
+//lastly use innerHTML to update the total cost
+
+var priceOfLetter = 5;
+var letterQuantity = 15;
+let totalCost = priceOfLetter * letterQuantity;
+
+document.getElementById("totalCost").innerHTML = totalCost;
+
